5dc634c6ad0add638902ef61f52519e9347ddb01
Isaad Khan <IKhan@lcs.on.ca>
2023-04-06 10:57:10 -0400

    Initial Commit

---
+++ MoodMapper/MoodMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperApp.swift
+//  MoodMapper
+//
+//  Created by Isaad Khan on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
